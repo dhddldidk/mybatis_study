b04d09c5952b2e2dec6ef7b6cbddd8d8f45ad08a
HyeSeon Yang <skcm19@naver.com>
2018-02-07 10:49:01 +0900

    mybatis_config.xml추가 application.properties추가 StudentMapper.xml추가 MyBatisWqlSessionFactory.java추가 MyBatisWqlSessionFactoryTest.java추가

---
+++ src/test/java/kr/or/dgit/mybatis_study/MyBatisSqlSessionFactoryTest.java
@@ -0,0 +1,28 @@
+package kr.or.dgit.mybatis_study;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import kr.or.dgit.mybatis_study.util.MyBatisSqlSessionFactory;
+
+public class MyBatisSqlSessionFactoryTest {
+	private static SqlSessionFactory factory;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		factory = MyBatisSqlSessionFactory.getSqlSessionFactory();
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		factory = null;
+	}
+	
+	@Test
+	public void test() {
+		Assert.assertNotNull(factory);
+	}
+}
